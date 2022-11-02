a2dd104425f9063bd0f11066d5508f32193d79fa
Koskay <68971733+Koskay@users.noreply.github.com>
2022-11-03 02:45:41 +0300

    refactoring config

--- app/database/config.py
+++ app/database/config.py
@@ -1,13 +1,13 @@
 from pydantic import BaseSettings, Field
 
-"""No docker. Раскоментировать"""
+"""If Not docker. Раскомментировать. Заполнить <...>"""
 # SQL_ALCHEMY_URL = f'postgresql://<ваш db_user>:<ваш db_pass>@<ваш db_host>:5432/<ваш db_name>'
 
 class Settings(BaseSettings):
-    """No docker. Закоментировать"""
+    """If Not docker. Закомментировать"""
     db_url: str = Field(..., env='DATABASE_URL')
         
-    """No docker. Раскоментировать"""
+    """If Not docker. Раскомментировать"""
     # db_url: str = SQL_ALCHEMY_URL)
 
 settings = Settings()
