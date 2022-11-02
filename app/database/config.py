be9cb486e2f58490a3419efc70653d4f183a90e9
Koskay <68971733+Koskay@users.noreply.github.com>
2022-11-03 02:30:01 +0300

    refacoring config

--- app/database/config.py
+++ app/database/config.py
@@ -1,21 +1,14 @@
-# import os
-# from dotenv import load_dotenv
-#
-# load_dotenv()
-#
-# DB_NAME = os.getenv('DB_NAME')
-# DB_HOST = os.getenv('DB_HOST')
-# DB_USER = os.getenv('DB_USER')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-#
-# SQL_ALCHEMY_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@db01:5432/postgres'
-
 from pydantic import BaseSettings, Field
 
+"""No docker. Раскоментировать"""
+# SQL_ALCHEMY_URL = f'postgresql://<ваш db_user>:<ваш db_pass>@<ваш db_host>:5432/<ваш db_name>'
 
 class Settings(BaseSettings):
+    """No docker. Закоментировать"""
     db_url: str = Field(..., env='DATABASE_URL')
-
+        
+    """No docker. Раскоментировать"""
+    # db_url: str = SQL_ALCHEMY_URL)
 
 settings = Settings()
 
